8240090396454c4070be621cf47a9c617616d006
Lali <micskeil@gmail.com>
2022-02-01 20:33:44 +0100

    test async messages

--- src/postMessageSocket.js
+++ src/postMessageSocket.js
@@ -16,6 +16,7 @@ export default class PostMessageSocket {
 	}
 
 	addListener(type, callback, { once = false } = {}) {
+		if (!this.#currentWindow) return;
 		this.#listeners[type] = { callback, once };
 	}
 
@@ -24,29 +25,22 @@ export default class PostMessageSocket {
 	}
 
 	sendMessage(type, payload) {
+		if (!this.#targetWindow) return;
 		const msgId = this.#getNextMsgId();
 		this.#targetWindow.postMessage(JSON.stringify({ type, payload, msgId }), "*");
 	}
 
 	sendRequest(type, payload) {
+		if (!this.#targetWindow) return;
 		const msgId = this.#getNextMsgId();
 		this.#targetWindow.postMessage(JSON.stringify({ type, payload, msgId }), "*");
-		this.#waitForResponse(msgId);
-	}
-
-	terminate() {
-		this.#appliedEventListeners.forEach((listener) => {
-			this.#currentWindow.removeEventListener("message", listener.handler);
-		});
-		this.#currentWindow = null;
-		this.#targetWindow = null;
+		return this.#waitForResponse(msgId);
 	}
 
 	#waitForResponse(msgId) {
 		return new Promise((resolve, reject) => {
 			const waitForResponse = (handlerFn, event) => {
 				if (event.source !== this.#targetWindow) return;
-
 				const index = this.#appliedEventListeners.findIndex(hadler => hadler._id === msgId);
 				if (index === -1) return;
 
@@ -54,7 +48,8 @@ export default class PostMessageSocket {
 					const response = this.#tryParse(event);
 					if (response.msgId !== msgId) return;
 
-					this.#currentWindow.removeEventListener("message", handlerFn);
+					event.stopPropagation();
+					this.#currentWindow.removeEventListener("message", handlerFn, true);
 					this.#appliedEventListeners.splice(index, 1);
 
 					if (response.error) {
@@ -63,14 +58,14 @@ export default class PostMessageSocket {
 						resolve(response.payload);
 					}
 				} catch (error) {
-					this.#currentWindow.removeEventListener("message", handlerFn);
+					this.#currentWindow.removeEventListener("message", handlerFn, true);
 					this.#appliedEventListeners.splice(index, 1);
 					reject(new Error(error));
 				}
 			};
 			const handler = waitForResponse.bind(this);
 			this.#currentWindow.addEventListener("message", waitForResponse.bind(this, handler));
-			this.#appliedEventListeners.push({ _id: msgId, handler });
+			this.#appliedEventListeners.push({ _id: msgId, handler, useCapture: true });
 		});
 	}
 
@@ -84,8 +79,8 @@ export default class PostMessageSocket {
 		this.#msgIdGenerator = msgIdGenerator.call(this);
 
 		const messageHandler = this.#onMessage.bind(this);
-		this.#currentWindow.addEventListener("message", messageHandler);
-		this.#appliedEventListeners.push({ _id: `START-${new Date().getTime()}`, hadler: messageHandler });
+		this.#currentWindow.addEventListener("message", messageHandler, false);
+		this.#appliedEventListeners.push({ _id: `START-${new Date().getTime()}`, handler: messageHandler, useCapture: false });
 	}
 
 	async #onMessage(event) {
@@ -134,5 +129,15 @@ export default class PostMessageSocket {
 
 	#getNextMsgId() {
 		return this.#msgIdGenerator.next().value;
+	}
+
+	terminate() {
+		if (!this.#currentWindow) return;
+		while (this.#appliedEventListeners.length) {
+			const listener = this.#appliedEventListeners.pop();
+			this.#currentWindow.removeEventListener("message", listener.handler, !!listener.useCapture);
+		}
+		this.#currentWindow = null;
+		this.#targetWindow = null;
 	}
 }
--- test/unit/postMessageSocket.spec.js
+++ test/unit/postMessageSocket.spec.js
@@ -24,7 +24,7 @@ function removeFixEvents(windowObject) {
 	windowObject.removeEventListener("message", fixEventsBinded);
 }
 
-describe("postMessageSocket", () => {
+describe("set up postMessageSocket environments", () => {
 	let pluginIframe;
 	const messages = [];
 	let windowSocket;
@@ -40,94 +40,216 @@ describe("postMessageSocket", () => {
 	const messageOne = "This is the first message";
 	const messageTwo = "This is the second message";
 
-	beforeAll(function () {
-		pluginIframe = document.createElement("iframe");
-		pluginIframe.src = "";
-		pluginIframe.allowFullscreen = "allowfullscreen";
-		body = document.querySelector("body");
-		body.appendChild(pluginIframe);
+	const sideEffects = {
+		document: {
+			addEventListener: {
+				fn: document.addEventListener,
+				refs: [],
+			},
+			keys: Object.keys(document),
+		},
+		window: {
+			addEventListener: {
+				fn: window.addEventListener,
+				refs: [],
+			},
+			keys: Object.keys(window),
+		},
+	};
 
-		windowSocket = new PostMessageSocket(window, pluginIframe.contentWindow);
-		addFixEvents(window, pluginIframe.contentWindow);
+	// Lifecycle Hooks
+	// -----------------------------------------------------------------------------
+	beforeAll(() => {
+		// Spy addEventListener
+		["document", "window"].forEach((obj) => {
+			const fn = sideEffects[obj].addEventListener.fn;
+			const refs = sideEffects[obj].addEventListener.refs;
 
-		iframeSocket = new PostMessageSocket(pluginIframe.contentWindow, window);
-		addFixEvents(pluginIframe.contentWindow, window);
+			function addEventListenerSpy(type, listener, options) {
+				// Store listener reference so it can be removed during reset
+				refs.push({ type, listener, options });
+				// Call original window.addEventListener
+				fn(type, listener, options);
+			}
+
+			// Add to default key array to prevent removal during reset
+			sideEffects[obj].keys.push("addEventListener");
+
+			// Replace addEventListener with mock
+			global[obj].addEventListener = addEventListenerSpy;
+		});
 	});
 
-	it("can sendMessage to partner trough the socket", async function () {
-		windowSocket.addListener(testWindowSocketOnce, messageCallback, { once: true });
-		iframeSocket.addListener(testiframeSocketSocketOnce, messageCallback, { once: true });
+	// Reset JSDOM. This attempts to remove side effects from tests, however it does
+	// not reset all changes made to globals like the window and document
+	// objects. Tests requiring a full JSDOM reset should be stored in separate
+	// files, which is only way to do a complete JSDOM reset with Jest.
+	beforeEach(() => {
+		const rootElm = document.documentElement;
 
-		windowSocket.sendMessage(testiframeSocketSocketOnce, messageOne);
-		iframeSocket.sendMessage(testWindowSocketOnce, messageTwo);
-		// we have to wait after all postMessage since they are implemented as setTimeout in jsdom
-		await new Promise(resolve => setTimeout(resolve, 100));
+		// Remove attributes on root element
+		[ ...rootElm.attributes ].forEach(attr => rootElm.removeAttribute(attr.name));
 
-		expect(messages).toHaveLength(2);
-		expect(messages[0]).toBe(messageOne);
-		expect(messages[1]).toBe(messageTwo);
+		// Remove elements (faster than setting innerHTML)
+		while (rootElm.firstChild) {
+			rootElm.removeChild(rootElm.firstChild);
+		}
+
+		// Remove global listeners and keys
+		["document", "window"].forEach((obj) => {
+			const refs = sideEffects[obj].addEventListener.refs;
+
+			// Listeners
+			while (refs.length) {
+				const { type, listener, options } = refs.pop();
+				global[obj].removeEventListener(type, listener, options);
+			}
+
+			// Keys
+			Object.keys(global[obj])
+				.filter(key => !sideEffects[obj].keys.includes(key))
+				.forEach((key) => {
+					delete global[obj][key];
+				});
+		});
+
+		// Restore base elements
+		rootElm.innerHTML = "<head></head><body></body>";
 	});
 
-	it("the eventlistener set up with parameter once is deleted after a message", async function () {
-		windowSocket.sendMessage(testiframeSocketSocketOnce, messageOne);
-		iframeSocket.sendMessage(testWindowSocketOnce, messageTwo);
-		// we have to wait after all postMessage since they are implemented as setTimeout in jsdom
-		await new Promise(resolve => setTimeout(resolve, 100));
+	describe("postMessageSocket tests", function () {
+		beforeEach(function () {
+			pluginIframe = document.createElement("iframe");
+			pluginIframe.src = "";
+			pluginIframe.allowFullscreen = "allowfullscreen";
+			body = document.querySelector("body");
+			body.appendChild(pluginIframe);
 
-		expect(messages).toHaveLength(2);
-		expect(messages[0]).toBe(messageOne);
-		expect(messages[1]).toBe(messageTwo);
-	});
+			addFixEvents(window, pluginIframe.contentWindow);
+			addFixEvents(pluginIframe.contentWindow, window);
+			windowSocket = new PostMessageSocket(window, pluginIframe.contentWindow);
+			iframeSocket = new PostMessageSocket(pluginIframe.contentWindow, window);
+		});
+		afterEach(async function () {
+			windowSocket.terminate();
+			await new Promise(resolve => setTimeout(resolve, 100));
+			windowSocket = null;
+			iframeSocket.terminate();
+			await new Promise(resolve => setTimeout(resolve, 100));
+			iframeSocket = null;
+			messages.length = 0;
+		});
 
-	it("can sendRequest to partner trough the socket", async function () {
-		windowSocket.addListener(testWindowSocket, messageCallback);
-		iframeSocket.addListener(testiframeSocketSocket, messageCallback);
+		it("can sendMessage to partner after the right listener set up which is DELETED afterwards", async function () {
+			windowSocket.addListener(testWindowSocketOnce, messageCallback, { once: true });
+			iframeSocket.addListener(testiframeSocketSocketOnce, messageCallback, { once: true });
 
-		windowSocket.sendRequest(testiframeSocketSocket, messageOne);
-		iframeSocket.sendRequest(testWindowSocket, messageTwo);
-		// we have to wait after all postMessage since they are implemented as setTimeout in jsdom
-		await new Promise(resolve => setTimeout(resolve, 100));
+			windowSocket.sendMessage(testiframeSocketSocketOnce, messageOne);
+			iframeSocket.sendMessage(testWindowSocketOnce, messageTwo);
+			// we have to wait after all postMessage since they are implemented as setTimeout in jsdom
+			await new Promise(resolve => setTimeout(resolve, 100));
 
-		expect(messages).toHaveLength(4);
-		expect(messages[2]).toBe(messageOne);
-		expect(messages[3]).toBe(messageTwo);
-	});
+			expect(messages).toHaveLength(2);
+			expect(messages[0]).toBe(messageOne);
+			expect(messages[1]).toBe(messageTwo);
 
-	it("the eventlistener set up with parameter without once is NOT deleted after a message", async function () {
-		windowSocket.sendRequest(testiframeSocketSocket, messageOne);
-		iframeSocket.sendRequest(testWindowSocket, messageTwo);
-		// we have to wait after all postMessage since they are implemented as setTimeout in jsdom
-		await new Promise(resolve => setTimeout(resolve, 100));
+			windowSocket.sendMessage(testiframeSocketSocketOnce, messageOne);
+			iframeSocket.sendMessage(testWindowSocketOnce, messageTwo);
+			// we have to wait after all postMessage since they are implemented as setTimeout in jsdom
+			await new Promise(resolve => setTimeout(resolve, 100));
 
-		expect(messages).toHaveLength(6);
-		expect(messages[4]).toBe(messageOne);
-		expect(messages[5]).toBe(messageTwo);
-	});
+			expect(messages).toHaveLength(2);
+			expect(messages[0]).toBe(messageOne);
+			expect(messages[1]).toBe(messageTwo);
+		});
 
-	it.todo("can set up hook");
+		it("can sendRequest to partner after the right listener set up which is NOT DELETED afterwards", async function () {
+			windowSocket.addListener(testWindowSocket, messageCallback);
+			iframeSocket.addListener(testiframeSocketSocket, messageCallback);
 
-	it("with a NOT matching message type the callback is not fired", async function () {
-		windowSocket.sendRequest("wrong msg type", messageOne);
-		iframeSocket.sendRequest("wrong msg type", messageTwo);
+			windowSocket.sendRequest(testiframeSocketSocket, messageOne);
+			iframeSocket.sendRequest(testWindowSocket, messageTwo);
+			// we have to wait after all postMessage since they are implemented as setTimeout in jsdom
+			await new Promise(resolve => setTimeout(resolve, 100));
 
-		await new Promise(resolve => setTimeout(resolve, 100));
-		expect(messages).toHaveLength(6);
-	});
+			expect(messages).toHaveLength(2);
+			expect(messages[0]).toBe(messageOne);
+			expect(messages[1]).toBe(messageTwo);
 
-	it("other windowObjects can not send msg", async function () {
-		removeFixEvents(window);
-		removeFixEvents(pluginIframe.contentWindow);
-		addFixEvents(window, window);
-		addFixEvents(pluginIframe.contentWindow, pluginIframe.contentWindow);
+			windowSocket.sendRequest(testiframeSocketSocket, messageOne);
+			iframeSocket.sendRequest(testWindowSocket, messageTwo);
+			// we have to wait after all postMessage since they are implemented as setTimeout in jsdom
+			await new Promise(resolve => setTimeout(resolve, 100));
 
-		// window.postMessage(JSON.stringify({ type: testWindowSocket, payload: messageOne, msgId: "testMsg" }), "*");
-		pluginIframe.contentWindow.postMessage(JSON.stringify({ type: testiframeSocketSocket, payload: messageOne, msgId: "testMsg" }), "*");
-		await new Promise(resolve => setTimeout(resolve, 100));
-		expect(messages).toHaveLength(6);
+			expect(messages).toHaveLength(4);
+			expect(messages[2]).toBe(messageOne);
+			expect(messages[3]).toBe(messageTwo);
+		});
 
-		removeFixEvents(window);
-		removeFixEvents(pluginIframe.contentWindow);
-		addFixEvents(window, pluginIframe.contentWindow);
-		addFixEvents(pluginIframe.contentWindow, window);
+		it("can send request and get answer from the opposite window object", async function () {
+			windowSocket.addListener("hook", (data) => {
+				return data + " from the other side.";
+			});
+			const answer = await iframeSocket.sendRequest("hook", "hello world");
+
+			expect(answer).toBe("hello world" + " from the other side.");
+
+			iframeSocket.addListener("hook", (data) => {
+				return data + " from this side.";
+			});
+			const answer2 = await windowSocket.sendRequest("hook", "hello world");
+
+			expect(answer2).toBe("hello world" + " from this side.");
+		});
+
+		it("with a NOT matching type the callback is not fired", async function () {
+			windowSocket.addListener(testWindowSocket, messageCallback);
+			iframeSocket.addListener(testiframeSocketSocket, messageCallback);
+
+			windowSocket.sendMessage("random-type", messageOne);
+			iframeSocket.sendMessage("random-type", messageTwo);
+
+			windowSocket.sendRequest("random-type", messageOne);
+			iframeSocket.sendRequest("random-type", messageTwo);
+
+			await new Promise(resolve => setTimeout(resolve, 100));
+			expect(messages).toHaveLength(0);
+		});
+
+		it("other windowObjects can not send msg", async function () {
+			removeFixEvents(window);
+			removeFixEvents(pluginIframe.contentWindow);
+			addFixEvents(window, window);
+			addFixEvents(pluginIframe.contentWindow, pluginIframe.contentWindow);
+
+			// window.postMessage(JSON.stringify({ type: testWindowSocket, payload: messageOne, msgId: "testMsg" }), "*");
+			pluginIframe.contentWindow.postMessage(JSON.stringify({ type: testiframeSocketSocket, payload: messageOne, msgId: "testMsg" }), "*");
+
+			await new Promise(resolve => setTimeout(resolve, 100));
+			expect(messages).toHaveLength(0);
+
+			removeFixEvents(window);
+			removeFixEvents(pluginIframe.contentWindow);
+			addFixEvents(window, pluginIframe.contentWindow);
+			addFixEvents(pluginIframe.contentWindow, window);
+		});
+
+		it.todo("return error from hooks");
+
+		it("test terminate", async function () {
+			windowSocket.terminate();
+
+			await new Promise(resolve => setTimeout(resolve, 100));
+
+			windowSocket.addListener(testWindowSocketOnce, messageCallback, { once: true });
+			iframeSocket.addListener(testiframeSocketSocketOnce, messageCallback, { once: true });
+
+			windowSocket.sendMessage(testiframeSocketSocketOnce, messageOne);
+			iframeSocket.sendMessage(testWindowSocketOnce, messageTwo);
+			// we have to wait after all postMessage since they are implemented as setTimeout in jsdom
+			await new Promise(resolve => setTimeout(resolve, 100));
+
+			expect(messages).toHaveLength(0);
+		});
 	});
 });
